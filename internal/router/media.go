daeea2e66750666a5b463ddf51ba751ebf33a32e
埃拉 <ElaBosak233@qq.com>
2024-03-26 17:50:03 +0800

    wip: remove old avatar service

--- internal/router/media.go
+++ internal/router/media.go
@@ -22,14 +22,6 @@ func NewMediaRouter(mediaRouter *gin.RouterGroup, mediaController controller.IMe
 }
 
 func (m *MediaRouter) Register() {
-	m.router.GET("/users/avatar/:id", m.controller.GetUserAvatarByUserId)
-	m.router.DELETE("/users/avatar/:id", m.controller.DeleteUserAvatarByUserId)
-	m.router.GET("/users/avatar/:id/info", m.controller.GetUserAvatarInfoByUserId)
-	m.router.POST("/users/avatar/:id", m.controller.SetUserAvatarByUserId)
-	m.router.GET("/teams/avatar/:id", m.controller.GetTeamAvatarByTeamId)
-	m.router.GET("/teams/avatar/:id/info", m.controller.GetTeamAvatarInfoByTeamId)
-	m.router.POST("/teams/avatar/:id", m.controller.SetTeamAvatarByTeamId)
-	m.router.DELETE("/teams/avatar/:id", m.controller.DeleteTeamAvatarByTeamId)
 	m.router.GET("/games/cover/:id", m.controller.GetGameCoverByGameId)
 	m.router.POST("/games/cover/:id", m.controller.SetGameCoverByGameId)
 	m.router.GET("/games/writeups/:id", m.controller.FindGameWriteUpByTeamId)
--- internal/service/media.go
+++ internal/service/media.go
@@ -4,17 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"github.com/elabosak233/cloudsdale/internal/config"
-	"io"
-	"mime/multipart"
 	"os"
-	"path"
 )
 
 type IMediaService interface {
-	SetUserAvatarByUserId(id uint, file *multipart.FileHeader) (err error)
-	GetUserAvatarByUserId(id uint) (dst string, err error)
-	GetUserAvatarInfoByUserId(id uint) (fileName string, fileSize int64, err error)
-	DeleteUserAvatarByUserId(id uint) (err error)
 	FindChallengeAttachmentByChallengeId(id int64) (err error)
 	CheckChallengeAttachmentByChallengeId(id int64) (fileName string, fileSize int64, err error)
 	DeleteChallengeAttachmentByChallengeId(id int64) (err error)
@@ -24,54 +17,6 @@ type MediaService struct{}
 
 func NewMediaService() IMediaService {
 	return &MediaService{}
-}
-
-func (a *MediaService) GetUserAvatarByUserId(id uint) (dst string, err error) {
-	fileName, _, err := a.GetUserAvatarInfoByUserId(id)
-	dst = path.Join(config.AppCfg().Gin.Paths.Media, "/users/avatar", fmt.Sprintf("/%d", id), fileName)
-	return dst, err
-}
-
-func (a *MediaService) GetUserAvatarInfoByUserId(id uint) (fileName string, fileSize int64, err error) {
-	p := path.Join(config.AppCfg().Gin.Paths.Media, "/users/avatar", fmt.Sprintf("/%d", id))
-	files, err := os.ReadDir(p)
-	if len(files) == 0 {
-		return "", 0, errors.New("无头像")
-	} else {
-		for _, file := range files {
-			if !file.IsDir() {
-				fileName = file.Name()
-				fileInfo, _ := file.Info()
-				fileSize = fileInfo.Size()
-			}
-		}
-		return fileName, fileSize, err
-	}
-}
-
-func (a *MediaService) SetUserAvatarByUserId(id uint, file *multipart.FileHeader) (err error) {
-	mime := file.Header.Get("Content-Type")
-	if mime != "image/jpeg" && mime != "image/png" {
-		return errors.New("图片格式错误")
-	}
-	p := path.Join(config.AppCfg().Gin.Paths.Media, "/users/avatar", fmt.Sprintf("/%d", id), file.Filename)
-	err = os.MkdirAll(path.Dir(p), os.ModePerm)
-	src, err := file.Open()
-	defer func(src multipart.File) {
-		_ = src.Close()
-	}(src)
-	dst, err := os.Create(p)
-	defer func(dst *os.File) {
-		_ = dst.Close()
-	}(dst)
-	_, err = io.Copy(dst, src)
-	return err
-}
-
-func (a *MediaService) DeleteUserAvatarByUserId(id uint) (err error) {
-	p := path.Join(config.AppCfg().Gin.Paths.Media, "/users/avatar", fmt.Sprintf("/%d", id))
-	err = os.RemoveAll(p)
-	return err
 }
 
 func (a *MediaService) FindChallengeAttachmentByChallengeId(id int64) (err error) {
