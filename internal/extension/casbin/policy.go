9851b1e1a8f41b08f857b27dd4c4466fa3202bcf
埃拉 <ElaBosak233@qq.com>
2024-06-06 10:24:29 +0800

    wip: policy of proxies

--- internal/extension/casbin/policy.go
+++ internal/extension/casbin/policy.go
@@ -52,6 +52,8 @@ func initDefaultPolicy() {
 		{"guest", "/api/users/login", "POST"},
 		{"guest", "/api/games/{id}/broadcast", "GET"},
 		{"guest", "/api/proxies/{id}", "GET"},
+		{"guest", "/api/proxies/{id}", "OPTIONS"},
+		{"guest", "/api/proxies/{id}", "POST"},
 		{"guest", "/api/media/*", "GET"},
 		{"guest", "/api/groups/", "GET"},
 	})
